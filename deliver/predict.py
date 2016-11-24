8fe5601fd70da273b6e69898d9478114e7307a93
blumonk <kim@rain.ifmo.ru>
2016-11-24 22:01:01 +0600

    add scripts

---
+++ deliver/predict.py
@@ -0,0 +1,37 @@
+import json
+import pandas as pd
+from train import to_binarize, to_scale, underscore
+
+def prepare_data(path):
+    """Convert data to match the format used on training.
+    """
+    clicks = pd.read_csv(path).rename(columns=underscore).drop('id', axis=1)
+    with open('dummies.json') as f:
+        tops = json.load(f)
+    for feature in to_binarize:
+        new_feature = lambda x: '{}_{}'.format(feature, x)
+        top = tops[feature]
+        for x in top:
+            clicks[new_feature(x)] = (clicks[feature] == x)
+        if len(clicks[feature].unique()) > len(top):
+            clicks[new_feature('other')] = 1 - sum(clicks[new_feature(col)] for col in top)
+
+    to_ord = lambda dt: dt.toordinal()
+    for date_col in [c for c in clicks.columns if c.startswith('date')]:
+        clicks[date_col] = pd.to_datetime(clicks[date_col]).apply(to_ord)
+
+    clicks.drop(to_binarize, axis=1, inplace=True)
+
+    with open('scale.json') as f:
+        mean_stds = json.load(f)
+
+    for feature in to_scale:
+        mu, std = mean_stds[feature]
+        clicks[feature] = (clicks[feature] - mu) / std
+
+    print(list(clicks.columns))
+    print(clicks[:10])
+
+
+if __name__ == '__main__':
+    prepare_data('click.csv')
---
+++ deliver/train.py
@@ -0,0 +1,98 @@
+import re
+import time
+import json
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_selection import SelectPercentile
+from sklearn.preprocessing import scale
+
+def underscore(name):
+    """Convert CamelCase string to python-style underscore_case."""
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+def add_dummies(dataset, features, top_ns):
+    """Add top-n binary features for each categorical feature.
+    
+    Arguments:
+    dataset -- the DataFrame object
+    features -- list of the categorical features to be converted
+    top_ns -- list of integers corresponding to the numbers of new binary features
+    """
+    assert len(features) == len(top_ns)
+    copy = dataset.copy()
+    tops = {}
+    for feature, top_n in zip(features, top_ns):
+        grouped = dataset.groupby(feature).size()
+        top = grouped.sort_values(ascending=False)[:top_n].index.values.tolist()
+        tops[feature] = top
+        new_feature = lambda x: '{}_{}'.format(feature, x)
+        for x in top:
+            copy[new_feature(x)] = (dataset[feature] == x)
+        if len(dataset[feature].unique()) > top_n:
+            copy[new_feature('other')] = 1 - sum(copy[new_feature(col)] for col in top)
+    with open('dummies.json', 'w') as f:
+        json.dump(tops, f)
+    return copy
+
+to_binarize = ['iata_from', 'iata_to', 'iso_from', 'iso_to', 'ak_from', 
+               'flight_class', 'combo', 'mobile', 'referer', 'combo_type']
+to_scale = ['date', 'price', 'total_price', 'a_days', 'days', 'date_from', 'date_back']
+
+def prepare_data(path):
+    """Preprocess dataset:
+    1) rename columns
+    2) convert dates to ordinals
+    3) binarize big categorical features
+    4) drop redundant features.
+    5) binarize remaining categorical features
+    6) scale numerical features
+    """
+    clicks = pd.read_csv(path).rename(columns=underscore).drop('id', axis=1)
+
+    to_ord = lambda dt: dt.toordinal()
+    for date_col in [c for c in clicks.columns if c.startswith('date')]:
+        clicks[date_col] = pd.to_datetime(clicks[date_col]).apply(to_ord)
+
+    prepared = add_dummies(clicks, to_binarize, [20] * 10)
+    prepared.drop(to_binarize, axis=1, inplace=True)
+    prepared = prepared[prepared.total_price < 2000000]
+
+    """
+    feature_values = {}
+    for feature in to_binarize:
+        dummies = pd.get_dummies(prepared[feature], prefix=feature)
+        feature_values[feature] = []
+        for new_col in dummies.columns:
+            feature_values[feature].append(new_col[len(feature) + 1:])
+        prepared = pd.concat([prepared.drop(feature, axis=1), dummies], axis=1)
+    with open('dummies.json', 'w') as f:
+        json.dump(feature_values, f)
+    """
+
+    mean_stds = {}
+    for feature in to_scale:
+        column = prepared[feature].astype(float)
+        mean_stds[feature] = (np.mean(column), np.std(column))
+        prepared[feature] = scale(column)
+    with open('scale.json', 'w') as f:
+        json.dump(mean_stds, f)
+
+    return prepared
+    
+def train(data):
+    gbc = GradientBoostingClassifier(random_state=10, n_estimators=50, verbose=1)
+    pipe = Pipeline([('selection', SelectPercentile()), ('gbc', gbc)])
+    params = {'selection__percentile': range(60, 101, 10), 'gbc__max_depth': range(3, 8)}
+    gsearch = GridSearchCV(estimator=pipe, param_grid=params, scoring='log_loss', n_jobs=2, verbose=1)
+    gsearch.fit(data.drop('book', axis=1), data.book)
+    print(gsearch.cv_results_)
+    return gsearch.best_estimator_
+
+if __name__ == '__main__':
+    start = time.time()
+    clicks = prepare_data('click.csv')
+    train(clicks)
